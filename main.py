dfb7641d55a484427942331b4cf2b3fda5947608
Iskander Khanov <needdocer@gmail.com>
2026-07-11 06:40:29 +0500

    ripear

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from fastapi import FastAPI
 from route import router
 from model import Base
 from database import engine
-Base.metadata.create_all(bind=engine)
 
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(router)
-
-
