b6d2dfdf39b19429ae87311adc3377004ca10265
Iskander Khanov <needdocer@gmail.com>
2026-07-12 22:30:41 +0500

    ADD CORS MIDDLEWARE

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from route import router
 from model import Base
 from database import engine
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 Base.metadata.create_all(bind=engine)
@@ -9,6 +10,15 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(router)
 
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='0.0.0.0', port=8000)
--- test/test_api.py
+++ test/test_api.py
@@ -2,10 +2,10 @@ import sys
 import os
 from pathlib import Path
 
-# Добавляем корневую папку проекта в PYTHONPATH
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 import os
+
 os.environ["DB_URL"] = "sqlite:///./test.db"
 
 # Самый простой вариант без фикстур
@@ -20,25 +20,19 @@ def test_create_url():
     mock_db = MagicMock()
     mock_db.query.return_value.filter.return_value.first.side_effect = [None, None]
 
-    # 2. Подменяем БД
     app.dependency_overrides[get_db] = lambda: mock_db
 
-    # 3. Тестируем
     client = TestClient(app)
     response = client.post("/post/url", json={"url": "https://example.com"})
 
-    # 4. Проверяем
     assert response.status_code == 200
 
-    # 5. Очищаем
     app.dependency_overrides.clear()
 
 
 def test_redirect():
-    # 1. Мок с существующим URL
     mock_db = MagicMock()
 
-    # Создаем объект URLS
     from model import URLS
     url_entry = URLS(id="abc123", url="https://example.com")
     mock_db.query.return_value.filter.return_value.first.return_value = url_entry
@@ -46,15 +40,12 @@ def test_redirect():
     # 2. Подменяем
     app.dependency_overrides[get_db] = lambda: mock_db
 
-    # 3. Тестируем
     client = TestClient(app)
     response = client.get("/abc123", follow_redirects=False)
 
-    # 4. Проверяем
     assert response.status_code == 302
     assert response.headers["location"] == "https://example.com"
 
-    # 5. Очищаем
     app.dependency_overrides.clear()
 
 
